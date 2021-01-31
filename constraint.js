ea7454e763588d573440e7874b40ef2bb714cf30
Kunal0905 <71588113+Kunal0905@users.noreply.github.com>
2021-01-31 19:46:16 +0530

    Add files via upload

---
+++ constraint.js
@@ -0,0 +1,12 @@
+class Rope{
+    constructor(body1,body2){
+        var op = {
+            bodyA: body1,
+            bodyB: body2,
+            length: 100,
+            stiffness: 0.54
+        }
+    this.rope = Matter.Constraint.create(op);
+    World.add(world,this.rope);
+    }
+}
